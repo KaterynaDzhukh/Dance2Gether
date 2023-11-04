b59fff130c98e799d53a31803dcea1fef9acce3c
Kateryna Dzhukh <132603306+KaterynaDzhukh@users.noreply.github.com>
2023-11-04 11:44:51 +0100

    Enitial setup Chat

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import 'dotenv/config';
 import client from "./db/db.js";
+import http from "http";
 import cors from "cors";
 import registrationRouter from "./Routes/registrationRouter.js"
-
+import {Server} from "socket.io";
 
 
 const app = express();
@@ -17,6 +18,19 @@ app.use("/api/registration", registrationRouter)
 app.get("/", (req, res) => {
     res.send ("Welcome to our Dance2Gether Chat")
 });
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {origin:"http://localhost:3000", methods: ["GET", "POST"]},
+});
+
+io.on("connection", (socket) => {
+    console.log(`a user connected ${socket.id}`);
+  
+    socket.on("send_message", (data) => {
+      socket.broadcast.emit("receive_message", data);
+    });
+  });
 
 app.use(
     cors({
