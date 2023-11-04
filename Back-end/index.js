bf3c8f636b28c836a7b136d48060568d7ce7f1ce
Kateryna Dzhukh <132603306+KaterynaDzhukh@users.noreply.github.com>
2023-11-04 15:48:25 +0100

    Fullstack last changes

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,15 +1,25 @@
 import express from "express";
 import 'dotenv/config';
 import client from "./db/db.js";
-import http from "http";
 import cors from "cors";
 import registrationRouter from "./Routes/registrationRouter.js"
-import {Server} from "socket.io";
+import socket from "./socket.js";
 
 
 const app = express();
+const server = socket();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET"],
+    allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"],
+    credentials: true,
+  })
+)
 
 app.use(express.json());
+
 
 
 app.use("/api/registration", registrationRouter)
@@ -19,27 +29,10 @@ app.get("/", (req, res) => {
     res.send ("Welcome to our Dance2Gether Chat")
 });
 
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {origin:"http://localhost:3000", methods: ["GET", "POST"]},
-});
 
-io.on("connection", (socket) => {
-    console.log(`a user connected ${socket.id}`);
-  
-    socket.on("send_message", (data) => {
-      socket.broadcast.emit("receive_message", data);
-    });
-  });
 
-app.use(
-    cors({
-      origin: "http://localhost:5173",
-      methods: ["GET"],
-      allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"],
-      credentials: true,
-    })
-  )
+
+
  
 
 
@@ -47,7 +40,7 @@ app.use(
   const port = process.env.PORT || 3000;
 
   client.on('connected', () => {
-      app.listen(port, () => {
+      server.listen(port, () => {
           console.log(`Server listening on port ${port}`)
       })
   })
