c0f79a5ff8af94729cc6ffdb064f328f74c7a85a
ZylaCode <zyla.verena@gmail.com>
2023-11-06 21:09:28 +0100

    gender model and search router

--- Back-end/Routes/searchRouter.js
+++ Back-end/Routes/searchRouter.js
@@ -2,12 +2,14 @@ import express from 'express';
 import User from '../models/UserModel.js';
 
 
+
 const searchRouter = express.Router();
 
-searchRouter.get("/", async (req, res)=> {
-
+searchRouter.get("/:cityId/:id", async (req, res)=> {
+    const cityId = req.params.cityId
+    const dancestyle = req.params.id
     try{
-    const response = await User.find();
+    const response = await User.find({dance_id: dancestyle, city_id: cityId});
     res.json(response)
 
   } catch(err){
