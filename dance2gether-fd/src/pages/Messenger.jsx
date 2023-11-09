fb0a0b8c942bd6b12f63042a5262ae3be6b28d7e
Anna Basarab <90757241+AnnaBasarab93@users.noreply.github.com>
2023-11-09 11:48:43 +0100

    deleted old version of Context

--- dance2gether-fd/src/pages/Messenger.jsx
+++ dance2gether-fd/src/pages/Messenger.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState,useContext, useRef } from "react";
+import { useEffect, useState,useContext } from "react";
 import Conversation from '../components/Conversation'
 import Message from "../components/Message";
 import { AuthContext } from "../contex/AuthContex";
