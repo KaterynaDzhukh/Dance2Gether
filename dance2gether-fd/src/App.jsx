efe1b4c60e028189c3a978fe8b2ee7c8ef99afce
Anna Basarab <90757241+AnnaBasarab93@users.noreply.github.com>
2023-11-09 21:24:25 +0100

    cleaned app.jsx

--- dance2gether-fd/src/App.jsx
+++ dance2gether-fd/src/App.jsx
@@ -1,83 +1,24 @@
 import "./App.css";
-<<<<<<< HEAD
-//import { useEffect, useState } from "react";
-//import io from "socket.io-client";
-//const socket = io.connect("http://localhost:3000");
-//import { useContext } from "react";
-import{BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-//import Messenger from "./pages/Messenger";
-//import Home from "./pages/Home";
-//import { AuthContext } from "./contex/AuthContex";
-import SearchUser from "../components/SearchUser";
-import Registration from "../components/Registration.jsx";
-import LogIn from "../components/LogIn";
+import{ Routes, Route, Navigate } from 'react-router-dom'
+import Messenger from "./pages/Messenger";
+import Home from "./pages/Home";
+import SearchUser from "./pages/SearchUser.jsx";
+import Registration from "./pages/Registration.jsx";
+import LogIn from "./pages/LogIn.jsx";
 
 function App() {
-  //const { user } = useContext(AuthContext);
 return (
       <>
-        <BrowserRouter>
     <Routes>
-        {/* <Route path="/" element={<Home/>} /> */}
-        {/* <Route path="/messenger" element= {user ? <Navigate to="/" /> : <Messenger />}/> */}
+        <Route path="/" element={<Home/>} /> 
+        <Route path="/messenger" element= { <Messenger />}/> 
         <Route path="/register" element={<Registration/>} />
         <Route path="/login" element={<LogIn/>} />
         <Route path="/searchUser" element={<SearchUser/>} />
   </Routes>
-  </BrowserRouter>
-  </>
-)}
-=======
-import { useContext } from "react";
-import{Routes, Route, Navigate } from 'react-router-dom'
-import Messenger from "./pages/Messenger";
-import Home from "./pages/Home";
-import { AuthContext } from "./contex/AuthContex";
-
-function App() {
-  const { user } = useContext(AuthContext);
-return (
-      <>
-        <div>
-    <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/messenger" element= {user ? <Navigate to="/" /> : <Messenger />}/>
-  </Routes>
-  </div>
   </>
 )}
 
-
- /* {const [message, setMessage] = useState('')
-  const [messageReceived, setMessageReceived] = useState("");
-  const sendMessage =()=> {
-    console.log("Button clicked");
-    socket.emit("send_message", { message: message });
-  }
-  
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
-    });
-  }, [socket]);
-
-  return (
-    <div className="App">
-      <input
-        placeholder="Message"
-        onChange={(e) => {
-          setMessage(e.target.value);
-        }}
-      />
-      <button onClick={sendMessage}>Send message</button>
-      <h1>
-        Message: {messageReceived}</h1>
-    </div>
-  );
-}}*/
-
-
->>>>>>> Anna_FrontEnd
 
 export default App;
 
