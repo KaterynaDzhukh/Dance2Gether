5b98c75b5a76e8fb699c5d127ebb2535662514cf
ncodena <c.odena.nuria@gmail.com>
2023-11-09 20:20:44 +0100

    removing commented code

--- dance2gether-fd/src/App.jsx
+++ dance2gether-fd/src/App.jsx
@@ -27,35 +27,5 @@ return (
   </>
 )}
 
-
-// function App() {
-//   const [message, setMessage] = useState('')
-//   const [messageReceived, setMessageReceived] = useState("");
-//   const sendMessage =()=> {
-//     console.log("Button clicked");
-//     socket.emit("send_message", { message: message });
-//   }
-  
-//   useEffect(() => {
-//     socket.on("receive_message", (data) => {
-//       setMessageReceived(data.message);
-//     });
-//   }, [socket]);
-
-//   return (
-//     <div className="App">
-//       <input
-//         placeholder="Message"
-//         onChange={(e) => {
-//           setMessage(e.target.value);
-//         }}
-//       />
-//       <button onClick={sendMessage}>Send message</button>
-//       <h1>
-//         Message: {messageReceived}</h1>
-//     </div>
-//   );
-// }
-
 export default App;
 
