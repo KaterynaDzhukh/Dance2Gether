e9b83b163ac9e436f9c54d4cde5b87ea656377fb
Anna Basarab <90757241+AnnaBasarab93@users.noreply.github.com>
2023-11-17 09:46:27 +0100

    email send message via works

--- dance2gether-fd/src/pages/UserProfile.jsx
+++ dance2gether-fd/src/pages/UserProfile.jsx
@@ -10,8 +10,10 @@ const UserProfile=() =>{
     const [error, setError] = useState(false);
     const [city, setCity] = useState([]);
     const [genders, setGenders] = useState([]);
-    const [dances, setDances] = useState({});
-const getUserProfile =async()=>{
+    const [dances, setDances] = useState([]);
+    const [email, setEmail] = useState([])
+
+    const getUserProfile =async()=>{
     try{
         const response =  await axios.get(`http://localhost:3000/api/profile/user/${id}`, 
         {headers: {'Content-Type':'application/json', 'Access-Control-Allow-Origin': '*' }})
@@ -21,6 +23,7 @@ const getUserProfile =async()=>{
         setCity(response.data.city_id)
         setDances(response.data.dance_id)
         setGenders(response.data.gender_id)
+        setEmail(response.data.email)
     }
 }catch(error){
     setError(true);
@@ -39,14 +42,15 @@ useEffect(()=>{
         <p>Loading...</p>) : (
     <>
 
-    
+<div>
+    <img src ={`${userInfo.profilePicture}`} width='30%' />
+        </div>
     <h2>{userInfo.userName}</h2>
     <div>
         <p>{city.cityName}</p>
     </div>
-        <div>
-    <img src ={`${userInfo.profilePicture}`} width='30%' />
-        </div>
+   
+      <button> <a target='_blank' href = {`mailto:${email}`}>Send Message</a></button>
     <div>
         <p>{genders.gender}</p>
     </div>
